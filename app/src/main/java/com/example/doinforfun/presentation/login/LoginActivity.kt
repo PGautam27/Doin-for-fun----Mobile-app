5478ae51d2aebf7e1a6ec5bf5df2a13473c77feb
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-08-22 22:50:52 +0530

    v1

--- app/src/main/java/com/example/doinforfun/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/example/doinforfun/presentation/login/LoginActivity.kt
@@ -1,28 +1,40 @@
-package com.example.doinforfun
+package com.example.doinforfun.presentation.login
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.doinforfun.presentation.login.LoginActivity
-import com.example.doinforfun.ui.theme.DoinForFunTheme
+import com.example.doinforfun.presentation.login.ui.theme.DoinForFunTheme
 
-class MainActivity : ComponentActivity() {
+class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DoinForFunTheme {
 
-                val intent = Intent(this@MainActivity, LoginActivity::class.java)
-                startActivity(intent)
             }
         }
     }
 }
+
+@Composable
+fun LoginScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
+}
+
+

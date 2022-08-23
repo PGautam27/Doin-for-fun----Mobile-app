bc835620874d073391ca68b5b70621e356cc666e
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-08-23 21:44:58 +0530

    v1

--- app/src/main/java/com/example/doinforfun/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/example/doinforfun/presentation/login/LoginActivity.kt
@@ -7,9 +7,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,12 +31,19 @@ class LoginActivity : ComponentActivity() {
 
 @Composable
 fun LoginScreen() {
+    val email = remember {
+        mutableStateOf(String())
+    }
+
+    val password = remember {
+        mutableStateOf(String())
+    }
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        OutlinedTextField(value = email.value, onValueChange ={email.value=it})
     }
 }
 

7467e6c8c0782d96cb1b78012557c0b22e8b5418
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-08-28 07:42:22 +0530

    v1

--- app/src/main/java/com/example/doinforfun/data/remote/DoinForFunApi.kt
+++ app/src/main/java/com/example/doinforfun/data/remote/DoinForFunApi.kt
@@ -1,5 +1,7 @@
-package com.example.doinforfun.data
+package com.example.doinforfun.data.remote
 
+import com.example.doinforfun.data.LoginRequest
+import com.example.doinforfun.data.LoginResponse
 import retrofit2.http.Body
 import retrofit2.http.POST
 
